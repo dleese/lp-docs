9ae906a9d5249ba016f07f460b89b66511ab8a82
Dirk Leese <dirk.leese@mac.com>
2020-05-11 15:29:17 +0200

    first project setup, add latex cover page

---
+++ eforms/source/conf.py
@@ -0,0 +1,226 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'LP-Docs'
+copyright = '2020, Dirk Leese'
+author = 'Dirk Leese'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+numfig = True
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_logo = '_images/logipad-aero-logo-black.png'
+headtextcolor =
+
+
+
+## Customized latex options
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_engine = 'pdflatex'
+latex_toplevel_sectioning = 'chapter'
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    'papersize': 'a4paper',
+    'releasename':" ",
+    # Sonny, Lenny, Glenn, Conny, Rejne, Bjarne and Bjornstrup
+    # 'fncychap': '\\usepackage[Lenny]{fncychap}',
+    'fncychap': '\\usepackage{fncychap}',
+    'fontpkg': '\\usepackage{amsmath,amsfonts,amssymb,amsthm}',
+
+    'figure_align':'htbp',
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    'pointsize': '12pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    'preamble': r'''
+        %%%%%%%%%%%%%%%%%%%% DD Settings %%%%%%%%%%%%%%%%%%
+        %%%add number to subsubsection 2=subsection, 3=subsubsection
+        %%% below subsubsection is not good idea.
+        \setcounter{secnumdepth}{3}
+        %
+        %%%% Table of content upto 2=subsection, 3=subsubsection
+        \setcounter{tocdepth}{2}
+
+        \usepackage{amsmath,amsfonts,amssymb,amsthm}
+        \usepackage{graphicx}
+        \usepackage[export]{adjustbox}
+
+        %%% reduce spaces for Table of contents, figures and tables
+        %%% it is used "\addtocontents{toc}{\vskip -1.2cm}" etc. in the document
+        \usepackage[notlot,nottoc,notlof]{}
+
+        \usepackage{color}
+        \usepackage{transparent}
+        \usepackage{eso-pic}
+        \usepackage{lipsum}
+
+        \usepackage{footnotebackref} %%link at the footnote to go to the place of footnote in the text
+
+        %% spacing between line
+        \usepackage{setspace}
+        %%%%\onehalfspacing
+        %%%%\doublespacing
+        \singlespacing
+
+
+        %%%%%%%%%%% datetime
+        \usepackage{datetime}
+
+        \newdateformat{MonthYearFormat}{%
+            \monthname[\THEMONTH], \THEYEAR}
+
+
+        %% RO, LE will not work for 'oneside' layout.
+        %% Change oneside to twoside in document class
+        \usepackage{fancyhdr}
+        \pagestyle{fancy}
+        \fancyhf{}
+
+        %%% Alternating Header for oneside
+        \fancyhead[L]{\ifthenelse{\isodd{\value{page}}}{ \small \nouppercase{\leftmark} }{}}
+        \fancyhead[R]{\ifthenelse{\isodd{\value{page}}}{}{ \small \nouppercase{\rightmark} }}
+
+        %%% Alternating Header for two side
+        %\fancyhead[RO]{\small \nouppercase{\rightmark}}
+        %\fancyhead[LE]{\small \nouppercase{\leftmark}}
+
+        %% for oneside: change footer at right side. If you want to use Left and right then use same as header defined above.
+        \fancyfoot[R]{\ifthenelse{\isodd{\value{page}}}{{\tiny Dirk Leese} }{\href{http://www.logipad.aerol}{\tiny visit Logipad ebsite }}}
+
+        %%% Alternating Footer for two side
+        %\fancyfoot[RO, RE]{\scriptsize Dirk Leese (dirk.leese@dextradata.com)}
+
+        %%% page number
+        \fancyfoot[CO, CE]{\thepage}
+
+        \renewcommand{\headrulewidth}{0.5pt}
+        \renewcommand{\footrulewidth}{0.5pt}
+
+        \RequirePackage{tocbibind} %%% comment this to remove page number for following
+        \addto\captionsenglish{\renewcommand{\contentsname}{Table of contents}}
+        \addto\captionsenglish{\renewcommand{\listfigurename}{List of figures}}
+        \addto\captionsenglish{\renewcommand{\listtablename}{List of tables}}
+        % \addto\captionsenglish{\renewcommand{\chaptername}{Chapter}}
+
+
+        %%reduce spacing for itemize
+        \usepackage{enumitem}
+        \setlist{nosep}
+
+        %%%%%%%%%%% Quote Styles at the top of chapter
+        \usepackage{epigraph}
+        \setlength{\epigraphwidth}{0.8\columnwidth}
+        \newcommand{\chapterquote}[2]{\epigraphhead[60]{\epigraph{\textit{#1}}{\textbf {\textit{--#2}}}}}
+        %%%%%%%%%%% Quote for all places except Chapter
+        \newcommand{\sectionquote}[2]{{\quote{\textit{``#1''}}{\textbf {\textit{--#2}}}}}
+    ''',
+
+
+    'maketitle': r'''
+        \pagenumbering{Roman} %%% to avoid page 1 conflict with actual page 1
+
+        \begin{titlepage}
+            \centering
+            \vspace*{0mm} %%% * is used to give space from top
+            \begin{figure}[!h]
+                \vspace*{-20mm}
+                \includegraphics[right]{/Users/dl/develop/lp-docs/eforms/source/_images/DD-logo.png}
+            \end{figure}
+            \vspace{30mm} %%% * is used to give space from top
+            \begin{figure}[!h]
+                \centering
+                \includegraphics[scale=1.0]{/Users/dl/develop/lp-docs/eforms/source/_images/logipad-aero-logo-black.png}
+            \end{figure}
+
+            \vspace{10mm} 
+            \textbf{\Huge {eForms User Guide}}
+
+            \vspace{30mm}
+            \Large \textbf{{Dirk Leese}}
+
+            \small Created on : \today
+
+            \vspace*{0mm}
+            \small  Last updated : \MonthYearFormat\today
+            
+            \vspace{70mm}
+            \begin{figure}[!h]
+                \hspace*{-20mm}
+                \includegraphics[scale=1.0,left]{/Users/dl/develop/lp-docs/eforms/source/_images/DD-stripes.png}
+            \end{figure}
+            %% \vfill adds at the bottom
+            \vfill
+            \small \textit{More information are available at }{\href{http://www.logipad.aero}{Logpad}}
+        \end{titlepage}
+
+        \clearpage
+        \pagenumbering{roman}
+        \tableofcontents
+        \listoffigures
+        \listoftables
+        \clearpage
+        \pagenumbering{arabic}
+
+        ''',
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+    'sphinxsetup': \
+        'hmargin={0.7in,0.7in}, vmargin={1in,1in}, \
+        verbatimwithframe=true, \
+        TitleColor={rgb}{0.443,0.160,0.282}, \
+        HeaderFamily=\\rmfamily\\bfseries, \
+        InnerLinkColor={rgb}{0.443,0.160,0.282}, \
+        OuterLinkColor={rgb}{0.443,0.160,0.282}',
+
+        'tableofcontents':' ',
+}
---
+++ eforms/source/eForms.rst
@@ -0,0 +1,14 @@
+eForms
+======
+Electronic forms have become more and more important as digitization and automatic data processing has progressed. eForms are not limited to simple text input or selection fields.
+
+eForms are not limited to simple text input or selection fields. Data input can be automated using the integrated QR code scanner, for example. No matter whether they record flight-relevant data or performance evaluations of service personnel. You have the option of evaluating the data via an individual dashboard or in Power BI. All entered data is stored on the device for offline use. Once a Wi-Fi or mobile data connection is established, completed eForm inputs can be sent to the backend system. Even workflows and notifications can be triggered to inform the relevant users/roles when, when eForms have been stored or have arrived on the server. Documents and photos can also be attached and forwarded via an appropriate eForm.
+
+.. figure::  ./_images/Logipad-eForms-web.png
+   :scale: 50
+
+   The eForm process
+
+eForm Generator
+---------------
+The Logipad eForms Module allows you to create your own electronic forms with our intuitive eForm drag and drop editor or use existing templates. The video shows how easy and intuitive it is to create eForms yourself with Logipad.
